98b087a33cdd00714c8127ca59166decd9bd8df6
JB <julien.bassin@avanade.com>
2023-04-16 11:19:43 +0200

    Adding Extensions refactoring serviceCollection and startup.cs file

--- Play.Catalog/src/Play.Catalog.Service/Startup.cs
+++ Play.Catalog/src/Play.Catalog.Service/Startup.cs
@@ -36,26 +36,11 @@ namespace Play.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeSerializer(MongoDB.Bson.BsonType.String));
-
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-            // this is an initialization point for those services can be used anywhere in the application
-            services.AddSingleton(serviceProvider =>
-            {
-                var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-                return mongoClient.GetDatabase(serviceSettings.ServiceName);
-            });
+            services.AddMongo();
 
-            services.AddTransient<IRepository<Item>>(serviceProvider =>
-            {
-                // Ask an instance of mongodb database
-                var database = serviceProvider.GetService<IMongoDatabase>();
-                return new MongoRepository<Item>(database, "Items");
-            });
-
+            services.AddMongoRepository<Item>("items");
 
             services.AddControllers(options =>
             {
