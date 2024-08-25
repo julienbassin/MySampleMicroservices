27934947feff5bcd8da9054ae79ce0d47f504c32
julien.bassin <julien.bassin@avanade.com>
2024-08-25 18:36:07 +0200

    updating source code for identity & frontend microservices

--- Play.Identity/src/Play.Identity.Service/Extensions.cs
+++ Play.Identity/src/Play.Identity.Service/Extensions.cs
@@ -1,17 +1,18 @@
 using Play.Identity.Service.Dtos;
 using Play.Identity.Service.Entities;
 
-namespace Play.Identity.Service;
-
-public static class Extensions
+namespace Play.Identity.Service
 {
-    public static UserDto AsDto(this ApplicationUser user)
+    public static class Extensions
     {
-        return new UserDto(
-            user.Id, 
-            user.UserName, 
-            user.Email, 
-            user.Gil, 
-            user.CreatedOn);
+        public static UserDto AsDto(this ApplicationUser user)
+        {
+            return new UserDto(
+                user.Id, 
+                user.UserName, 
+                user.Email, 
+                user.Gil, 
+                user.CreatedOn);
+        }
     }
 }
